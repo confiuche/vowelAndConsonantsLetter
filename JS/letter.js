d6f834b3f9b70301b430f2d839c9a9a5fa632c56
Irechukwu Confidence <irechukwuconfidence@gmail.com>
2021-09-07 20:29:10 +0100

    .

---
+++ JS/letter.js
@@ -0,0 +1,38 @@
+//let vow = ['a', 'e', 'i', 'o', 'u']
+function classifyLetters(word){
+    //vow = 'aeiou'
+    let vow = ['a', 'e', 'i', 'o', 'u']
+    conson = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+     newWord = word.split('')
+    //  dd=[];
+    //  dd.push(vow)
+    //  console.log(dd)
+     //newVowel = vow.split("")
+     //console.log(word)
+     console.log(newWord)
+     //console.log(newVowel)
+    consonants = [];
+    vowels = [];
+    display = {
+        vowels : vowels,
+        consonants : consonants
+    }
+     for(let i = 0; i<word.length; i++){
+      let any = word[i];
+       
+        //let aa = any.split("")
+        //console.log(any)
+        if(vow.includes(any)){
+            display.vowels.push(any)
+        }else{
+            display.consonants.push(any)
+        }
+   }
+    return display;
+    //word.split('')
+    
+}
+ //word = ["bastion"]
+console.log(classifyLetters('bastion'))
+
+
